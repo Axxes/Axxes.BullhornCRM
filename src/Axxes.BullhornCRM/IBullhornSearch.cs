d7e03a798eb5b42fea5258e4e37385c195678fed
Ruben Vervust <Ruben.Vervust@axxes.com>
2023-12-08 15:42:18 +0100

    Add pagination parameters to search methods

--- src/Axxes.BullhornCRM/IBullhornSearch.cs
+++ src/Axxes.BullhornCRM/IBullhornSearch.cs
@@ -7,21 +7,21 @@ namespace Axxes.BullhornCRM;
 public interface IBullhornSearch<T> where T : IBullhornEntity
 {
     [Post("")]
-    internal Task<BullhornRecords<T>> FindInternal([Body] BullhornFind query, [Query] string fields = "*",
+    internal Task<BullhornRecords<T>> FindInternal([Body] BullhornFind query, [Query] string fields = "*", [Query] int count = 500, [Query] int start = 0,
         CancellationToken cancellationToken = default);
 
-    public async Task<BullhornRecords<T>> FindAll(Dictionary<string, string> properties, [Query] string fields = "*",
+    public async Task<BullhornRecords<T>> FindAll(Dictionary<string, string> properties, string fields = "*", int count = 500, int start = 0,
         CancellationToken cancellationToken = default)
     {
         var body = new BullhornFind(properties);
 
-        return await FindInternal(body, fields, cancellationToken);
+        return await FindInternal(body, fields, count, start, cancellationToken);
     }
 
-    public async Task<T> Find(Dictionary<string, string> properties, [Query] string fields = "*",
+    public async Task<T> Find(Dictionary<string, string> properties, string fields = "*", int count = 500, int start = 0, 
         CancellationToken cancellationToken = default)
     {
-        var result = await FindAll(properties, fields, cancellationToken);
+        var result = await FindAll(properties, fields, count, start, cancellationToken);
         return result.Data.FirstOrDefault();
     }
 }
